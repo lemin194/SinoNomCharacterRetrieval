7122b7b334134c39f2b160f16d35a8163051bf04
Quannggg <nhieuvai@gmail.com>
2024-05-21 17:25:50 +0700

    Transform and Generate data

--- classification/transform.py
+++ classification/transform.py
@@ -1,15 +1,19 @@
 import albumentations as alb
 from albumentations.pytorch import ToTensorV2
 from albumentations.augmentations.geometric.resize import Resize
+from albumentations.augmentations.dropout.coarse_dropout import CoarseDropout
 
 train_transform = alb.Compose(
     [
         Resize(64, 64),
         alb.Compose(
+            
             [alb.ShiftScaleRotate(shift_limit=(-.1, .1), scale_limit=(-.15, 0), rotate_limit=5, border_mode=0, interpolation=3,
                                   value=[255, 255, 255], p=1),
              alb.GridDistortion(distort_limit=0.1, border_mode=0, interpolation=3, value=[255, 255, 255], p=0.5)], p=0.5),
+            
         alb.InvertImg(p=.3),
+        alb.CoarseDropout(max_holes=3, max_height=8, max_width=8, min_holes=1, min_height=8, min_width=8, p=0.5),
         # alb.Blur(blur_limit=3, p=0.5),
         # alb.RGBShift(r_shift_limit=15, g_shift_limit=15,
         #              b_shift_limit=15, p=0.3),
@@ -19,7 +23,7 @@ train_transform = alb.Compose(
         alb.ToGray(always_apply=True),
         alb.Normalize((0.6281) * 3, (0.4123) * 3),
         # alb.Sharpen()
-        ToTensorV2(False),
+        # ToTensorV2(False),
     ]
 )
 
