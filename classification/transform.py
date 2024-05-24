28be1905fa4bb20fdf558792a7f9b8184f87702e
lemin194 <leminhduc190204@gmail.com>
2024-05-24 14:22:20 +0700

    Change transform

--- classification/transform.py
+++ classification/transform.py
@@ -7,23 +7,23 @@ train_transform = alb.Compose(
     [
         Resize(64, 64),
         alb.Compose(
-            
-            [alb.ShiftScaleRotate(shift_limit=(-.1, .1), scale_limit=(-.15, 0), rotate_limit=5, border_mode=0, interpolation=3,
-                                  value=[255, 255, 255], p=1),
+            [alb.ShiftScaleRotate(shift_limit=(-.1, .1), scale_limit=(-.2, .2), rotate_limit=5, border_mode=0, interpolation=3,
+                                  value=[255, 255, 255], p=.5),
              alb.GridDistortion(distort_limit=0.1, border_mode=0, interpolation=3, value=[255, 255, 255], p=0.5)], p=0.5),
-            
         alb.InvertImg(p=.3),
-        alb.CoarseDropout(max_holes=3, max_height=8, max_width=8, min_holes=1, min_height=8, min_width=8, p=0.5),
+        CoarseDropout(max_holes=6,max_height=6,max_width=6,
+                      min_holes=3,min_height=3,min_width=3,
+                      fill_value=255, p=.3), 
         # alb.Blur(blur_limit=3, p=0.5),
         # alb.RGBShift(r_shift_limit=15, g_shift_limit=15,
         #              b_shift_limit=15, p=0.3),
-        alb.GaussNoise(var_limit=100, mean=0, p=0.5),
-        alb.RandomBrightnessContrast(.05, (-.2, 0), True, p=0.2),
+        alb.GaussNoise(var_limit=200, mean=0, p=0.5),
+        alb.RandomBrightnessContrast(.2, (-.2, .2), True, p=0.2),
         alb.ImageCompression(95, p=.3),
         alb.ToGray(always_apply=True),
         alb.Normalize((0.6281) * 3, (0.4123) * 3),
         # alb.Sharpen()
-        # ToTensorV2(False),
+        ToTensorV2(False),
     ]
 )
 
@@ -32,6 +32,26 @@ test_transform = alb.Compose(
         Resize(64, 64),
         alb.ToGray(always_apply=True),
         alb.Normalize((0.6281) * 3, (0.4123) * 3),
+        # alb.Sharpen()
+        ToTensorV2(False),
+    ]
+)
+
+db_transform = alb.Compose(
+    [
+        Resize(64, 64),
+        alb.ToGray(always_apply=True),
+        alb.Normalize((0.8481) * 3, (0.3321) * 3),
+        # alb.Sharpen()
+        ToTensorV2(False),
+    ]
+)
+
+query_transform = alb.Compose(
+    [
+        Resize(64, 64),
+        alb.ToGray(always_apply=True),
+        alb.Normalize((0.7972) * 3, (0.3157) * 3),
         # alb.Sharpen()
         ToTensorV2(False),
     ]
